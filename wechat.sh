7845fcd2c49dfeb06edee95d4b3524a36ba94438
Shengjing Zhu <i@zhsj.me>
2026-09-08 22:26:29 +0800

    Don't set IME env on Wayland
    And drop QT_QPA_PLATFORM hack
    
    Updates: #98

--- wechat.sh
+++ wechat.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 setup_ime_env() {
-  if [[ "$XMODIFIERS" =~ fcitx ]]; then
+  if [[ "$XMODIFIERS" =~ fcitx && -z "$WAYLAND_DISPLAY" ]]; then
     [ -z "$QT_IM_MODULE" ] && export QT_IM_MODULE=fcitx
     [ -z "$GTK_IM_MODULE" ] && export GTK_IM_MODULE=fcitx
-  elif [[ "$XMODIFIERS" =~ ibus ]]; then
+  elif [[ "$XMODIFIERS" =~ ibus && -z "$WAYLAND_DISPLAY" ]]; then
     [ -z "$QT_IM_MODULE" ] && export QT_IM_MODULE=ibus
     [ -z "$GTK_IM_MODULE" ] && export GTK_IM_MODULE=ibus
   fi
