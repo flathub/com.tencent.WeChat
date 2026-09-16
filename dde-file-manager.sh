dbab77961e83baf0b83089e1a9d5b21bb0e38496
Shengjing Zhu <i@zhsj.me>
2026-09-16 22:09:50 +0800

    Use org.freedesktop.portal.OpenURI.OpenDirectory to open file

--- dde-file-manager.sh
+++ dde-file-manager.sh
@@ -2,11 +2,14 @@
 
 if [ "$1" == "--show-item" ]; then
   real_path=$(realpath -m "$2" | sed "s:^$HOME/xwechat_files/:$HOME/.var/app/$FLATPAK_ID/xwechat_files/:")
-  echo "real path is $real_path"
+
   if [ -f "$real_path" ]; then
+    echo "Calling org.freedesktop.portal.OpenURI.OpenDirectory $real_path"
     gdbus call --session \
-      --dest org.freedesktop.FileManager1 --object-path /org/freedesktop/FileManager1 \
-      --method org.freedesktop.FileManager1.ShowItems "['file://$real_path']" "" >/dev/null
+      --dest org.freedesktop.portal.Desktop \
+      --object-path /org/freedesktop/portal/desktop \
+      --method org.freedesktop.portal.OpenURI.OpenDirectory \
+      "" 3 '{}' 3< "$real_path" >/dev/null
   else
     zenity --error --text "Path $real_path not found" --icon-name com.tencent.WeChat
   fi
